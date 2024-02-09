0a5d02c6fcf12768997f4eaef001eff13f69a50b
Rafaarias12 <58712651+Rafaarias12@users.noreply.github.com>
2024-02-09 17:23:18 -0500

    Edición y eliminación de paises.

--- Sales.Web/Repositories/IRepository.cs
+++ Sales.Web/Repositories/IRepository.cs
@@ -1,4 +1,6 @@
-﻿namespace Sales.Web.Repositories
+﻿using System.Text.Json;
+
+namespace Sales.Web.Repositories
 {
     public interface IRepository
     {
@@ -7,6 +9,10 @@
         Task<HttpResponseWrapper<object>> Post<T>(string url, T model);
 
         Task<HttpResponseWrapper<TResponse>> Post<T, TResponse>(string url, T model);
+        //Task<T> UnserializeAnswer<T>(HttpResponseMessage httpResponse, JsonSerializerOptions jsonSerializerOptions);
+        Task<HttpResponseWrapper<object>> Delete(string url);
+        Task<HttpResponseWrapper<object>> Put<T>(string url, T model);
+        Task<HttpResponseWrapper<TResponse>> Put<T, TResponse>(string url, T model);
     }
 
 }
